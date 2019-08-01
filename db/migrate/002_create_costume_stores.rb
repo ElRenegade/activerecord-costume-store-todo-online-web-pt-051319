36dcd7dfaa74322bfa7a1ef579a6ce3926f26f7d
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-01 04:57:12 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |r|
+      r.text :name
+      r.text :location
+      r.integer :costume_inventory
+      r.integer :num_of_employees
+      r.boolean :still_in_business
+      r.datetime :opening_time
+      r.datetime :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |r|
+      r.text :name
+      r.text :location
+      r.text :theme
+      r.integer :price
+      r.boolean :family_friendly
+      r.datetime :opening_date
+      r.datetime :closing_date
+      r.text :description
+    end
+  end
+end
