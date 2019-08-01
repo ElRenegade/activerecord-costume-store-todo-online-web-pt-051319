1ff97d334de3eac646dedcc8ba93b781d7632e19
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-01 04:40:42 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
 # Create your costume_stores migration here
+class CostumeStores
+  def change
+    create_table :costume_stores do |r|
+      r.text :name
+      r.text :location
+      r.integer :number_of_costumes
+      r.integer :number_of_employees
+      r.boolean :still_in_business
+      r.datetime :opening_time
+      r.datetime :colsing_time
+    end
+  end
+end
