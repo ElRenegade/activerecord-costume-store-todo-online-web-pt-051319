55725f2422d4b73c8a7f3357f6708203d903992b
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-01 04:56:23 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -5,7 +5,7 @@ class CreateCostumeStores < ActiveRecord::Migration[5.2]
       r.text :name
       r.text :location
       r.integer :costume_inventory
-      r.integer :employee_count
+      r.integer :num_of_employees
       r.boolean :still_in_business
       r.datetime :opening_time
       r.datetime :closing_time
