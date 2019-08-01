1d64bab2bd60a47486c2feb8481ebfece0031755
Santiago Sanchez <chagostwo@yahoo.com>
2019-08-01 04:43:59 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class HauntedHouse < ActiveRecord::Migration[5.2]
+  def change
+    create_table :haunted_houses do |r|
+      r.text :name
+      r.text :location
+      r.text :theme
+      r.integer :price
+      r.boolean :family_friendly
+      r.datetime :opening_date
+      r.datetime :colsing_date
+      r.text :long_description
+    end
+  end
+end
